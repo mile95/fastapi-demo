58a3152596b1f17cdb13b89b4d4d3f0e6a0eb1d5
Fredrik Mile <fredrik.mile@gmail.com>
2021-10-12 21:40:09 +0200

    Black formatting

--- improved_methods.py
+++ improved_methods.py
@@ -2,30 +2,26 @@ from pydantic.networks import EmailStr
 
 
 @app.put("/user", response_model=User)
-def add_new_user(user:User) -> str:
+def add_new_user(user: User) -> str:
     if user.username in users:
         raise HTTPException(status_code=409, detail=f"{user.username} already exists!")
     users[user.username] = user.email
-    return {
-        'username': user.username,
-        'email': user.email
-    }
+    return {"username": user.username, "email": user.email}
+
 
 @app.get("/user", response_model=User)
-def get_user(username:str):
+def get_user(username: str):
     if username not in users:
-        raise HTTPException(status_code=404, detail=f'{username} does not exits')
-    return {
-        "user": username,
-        "email": users[username]
-    }
+        raise HTTPException(status_code=404, detail=f"{username} does not exits")
+    return {"user": username, "email": users[username]}
+
 
 @app.delete("/user")
 def delete_user(username):
     if username not in users:
-        raise HTTPException(status_code=404, detail=f'{username} does not exists!')
+        raise HTTPException(status_code=404, detail=f"{username} does not exists!")
     del users[username]
-    return f'{username} deleted!'
+    return f"{username} deleted!"
 
 
 class User(BaseModel):
